a40400abb7bbe99c021088b602fd9852ad39f31b
eee27 <liu.bo27@stemsoft.com.cn>
2021-01-05 16:30:57 +0800

    fix

--- src/demorcp/Perspective.java
+++ src/demorcp/Perspective.java
@@ -7,6 +7,6 @@ public class Perspective implements IPerspectiveFactory {
 
 	@Override	
 	public void createInitialLayout(IPageLayout layout) {
-		layout.setEditorAreaVisible(false);
+		layout.setEditorAreaVisible(true);
 	}
 }
--- src/demorcp/views/HelloWorldView.java
+++ src/demorcp/views/HelloWorldView.java
@@ -1,4 +1,4 @@
-package demorcp;
+package demorcp.views;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
